9f0bca11dc21de48f9561938f9565db2834df3d8
ajkendal <15990675+ajkendal@users.noreply.github.com>
2025-02-07 09:32:02 -0700

    module 2.1

---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import "./styles/nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img
+            src="\static\images\LittleLemonLogo.png"
+            alt="Little Lemon Logo"
+          />
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order-online">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
